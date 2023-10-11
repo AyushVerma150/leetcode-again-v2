a5784ea8178ea062f30fc0c00dc96cce2482fe3b
Ayush <=>
2023-10-11 05:59:17 +0530

    Revision of Longest Sub-Array Zero sum

--- Longest Subarray with Zero Sum/Optimal_Approach.cpp
+++ Longest Subarray with Zero Sum/Optimal_Approach.cpp
@@ -2,26 +2,30 @@
 
 // Extra Space : O(n)
 // Time Complexity : O(n)
-
-int getLongestZeroSumSubarrayLength(vector<int> &arr) {
+int getLongestZeroSumSubarrayLength(vector<int> &arr)
+{
   int size = arr.size();
-  unordered_map<int,int> mp;
+  unordered_map<int, int> mp;
 
   int prefix_sum = 0;
   int max_count = INT_MIN;
 
-  for(int i=0;i<size;i++){
-    prefix_sum+=arr[i];
+  for (int i = 0; i < size; i++)
+  {
+    prefix_sum += arr[i];
     // If the Prefix Sum gets you a sum of "0" !!
-    if(prefix_sum == 0){
-      max_count = max(max_count, i+1);
+    if (prefix_sum == 0)
+    {
+      max_count = max(max_count, i + 1);
     }
-    if(mp.find(prefix_sum) != mp.end()){
+    if (mp.find(prefix_sum) != mp.end())
+    {
       // we found the x, 0 sum is there.
       int index = mp[prefix_sum];
       max_count = max(max_count, i - index);
     }
-    else{
+    else
+    {
       // the sum will be added to the map!
       mp[prefix_sum] = i;
     }
@@ -29,5 +33,3 @@ int getLongestZeroSumSubarrayLength(vector<int> &arr) {
 
   return max_count;
 }
-
-
